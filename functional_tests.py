b87f7894a7f4941de5f04d8b41255f99ae4cd042
gyro <gyrcl@outlook.com>
2014-12-28 21:46:05 -0800

    Refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,18 +18,34 @@ class NewVisitorTest(unittest.TestCase):
 
         #She notes the page title and header mention to-do Lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the Test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         #She types "Buy peacock feathers" into a text box
-
+        inputbox.send_keys('Buy peacock feathers')
+        
         #when she hits enter, the page updates and now the page lists
         #"1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
         #There is still a text box inviting her to add another item.  She
-        #enters "Use peacock features to make a fly" (Lisa is Methodical)
-
+        #enters "Use peacock features to make a fly" (Lisa is Methodical)    
+        self.fail('Finish the Test!')
+        
+        
         #The page updates again, and now shows both items on her list
 
         #Lisa wonder whether the site will remember her list.  Then she sees
