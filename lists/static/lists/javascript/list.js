aefaa27064887994f2689bbd6aabd91d17970928
gyro <gyrcl@outlook.com>
2015-03-31 22:43:44 -0700

    get rid of error message when onclick happens

--- lists/static/lists/javascript/list.js
+++ lists/static/lists/javascript/list.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
-    $('input').on("keypress",function(event){
-       $('.has-error').hide(); 
-    });; 
+jQuery(document).ready(function($) {
 });
+
+function bind_key_and_click_handler() {
+    $('#id_text').on("keypress",function(event){
+          $('.has-error').hide(); 
+    });
+    
+    $('#id_text').click(function(event){
+          $('.has-error').hide(); 
+       });
+}; 
